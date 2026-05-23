68ddd71ba48febde3368439281d510ef29d08e36
Kim-Chan Tze Hui, Louiz <34767148+zioul123@users.noreply.github.com>
2026-05-23 21:57:25 +0100

    Refactor first-pool optimizer into focused pipeline modules

---
+++ src/ml/worker/pipelines/optimization/first-pool/types.ts
@@ -0,0 +1,32 @@
+import type { createTensor } from "../../../../index";
+import type { GpuBufferRef, OwnedGpuBuffer } from "../../../runtime/bufferKernels";
+
+export type TensorShape4D = readonly [number, number, number, number];
+
+export type FirstPoolPersistentContext = {
+  conv1Weight: ReturnType<typeof createTensor>;
+  conv2Weight: ReturnType<typeof createTensor>;
+  conv3Weight: ReturnType<typeof createTensor>;
+  styleGram1Buffer: GpuBufferRef;
+  styleGram3Buffer: GpuBufferRef;
+  contentRelu2Buffer: GpuBufferRef;
+  dispose: () => void;
+};
+
+export type FirstPoolStepResult = {
+  nextInputBuffer: OwnedGpuBuffer;
+  totalLoss: number;
+  forwardMs: number;
+  lossMs: number;
+  backwardMs: number;
+  updateMs: number;
+  readbackMs: number;
+};
+
+export type FirstPoolStatsAccumulator = {
+  forwardMs: number;
+  lossMs: number;
+  backwardMs: number;
+  updateMs: number;
+  readbackMs: number;
+};
